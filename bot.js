b2b9f2ba9bd5c151123722d6e35f2b916a734d45
NFSytra <41764728+NFSytra@users.noreply.github.com>
2018-08-01 01:53:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,9 +8,6 @@ const fetchVideoInfo = require('youtube-info');
 
 const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
 const prefix = '+';
-client.on('ready', function() {
-    console.log(`i am ready ${client.user.username}`);
-});
 client.on('ready', () => {
 console.log('Logging into discord..');
 console.log(`
